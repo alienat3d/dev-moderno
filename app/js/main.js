da1cf8d7d02d91dd078c8233bae812c61e01e07a
alienat3d <psy.alienated@gmail.com>
2021-06-06 14:30:26 +0300

    added 'product-slider' section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
 $(function () {
+
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    normalFill: "#f4f6f5",
+    ratedFill: "#ffa726"
+  });
+
+  $(".product-slider__inner").slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: false,
+    dots: true,
+  });
+
   var mixer = mixitup(".products__products-wrapper");
+
 })
