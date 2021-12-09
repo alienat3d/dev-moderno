5dda5c5b315ab6b85c60c7591d3f669ec961c4e1
alienat3d <psy.alienated@gmail.com>
2021-12-10 02:35:50 +0300

    started adaptive of page.html

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,18 @@
 $(function () {
-
-  $(".rate-star").rateYo({
+  $('.rate-star').rateYo({
     rating: 5,
-    starWidth: "12px",
-    normalFill: "#f4f6f5",
-    ratedFill: "#ffa726"
+    starWidth: '12px',
+    normalFill: '#f4f6f5',
+    ratedFill: '#ffa726',
   });
 
-  $(".product-slider__inner").slick({
+  $('.product-slider__inner').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     arrows: false,
     dots: true,
   });
 
-  var mixer = mixitup(".products__products-wrapper");
-
-})
+  // пишем всегда последним, потому что он написан чистом JavaScript и если на какой-то странице он не находит свой элемент, то всё, что ниже него ломается. Поэтому ставим его последним.
+  var mixer = mixitup('.products__products-wrapper');
+});
